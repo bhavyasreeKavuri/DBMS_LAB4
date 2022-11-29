5a731d480b43ca090a018acea92ab10f721be364
bhavyasreeKavuri <111688424+bhavyasreeKavuri@users.noreply.github.com>
2022-11-29 21:17:15 +0530

    Delete 5.Supplier Details.sql

--- 5.Supplier Details.sql
+++
@@ -1,4 +0,0 @@
-select SUP.SUPP_ID , SUPP_NAME , SUPP_PHONE , SUPP_CITY FROM supplier as sup
-inner join 
-( select * from supplier_pricing group by SUPP_ID having count(SUPP_ID)>1) as sp
-on sp.SUPP_ID=sup.SUPP_ID;
